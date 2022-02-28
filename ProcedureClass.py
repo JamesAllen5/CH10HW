0d9277cc0d211477c1a3e5dc49ec6d64cd385f38
James Allen <james_allen2@baylor.edu>
2022-02-28 17:10:21 -0600

    hw progress 5:10

---
+++ ProcedureClass.py
@@ -0,0 +1,29 @@
+class Procedure:
+    def __init__(self,procedure_name, procedure_date, practitioner_name, charge,patient_id):
+        self.__procedure_name=procedure_name
+        self.__procedure_date=procedure_date
+        self.__practitioner_name=practitioner_name
+        self.__charge=charge
+        self.__patient_id=patient_id
+
+    def get_procedure_name(self):
+        return self.__procedure_name
+    
+    def get_procedure_date(self):
+        return self.__procedure_date
+
+    def get_practitioner_name(self):
+        return self.__practitioner_name
+
+    def get_charge(self):
+        return self.__charge
+
+    def get_patient_id(self):
+        return self.__patient_id
+
+    def print_procedure(self):
+        if self.__patient_id==1:
+            print("Procedure: ",self.__procedure_name)
+            print("Date: ",self.__procedure_date)
+            print("Practitioner: ",self.__practitioner_name)
+            print("Charge: ",self.__charge)
